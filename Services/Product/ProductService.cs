cfdcb3c52ff36e72b5856bb98c7e61d7c29f359c
gabriel.peixoto <desenvolvimento14@resolveconsultoria.com>
2023-04-25 11:57:52 -0300

    feat: add service para insetir produtos

---
+++ Services/Product/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleStorage.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStorage.Services.Product
+{
+    public class ProductService
+    {
+        ApplicationData context;
+
+        public ProductService(ApplicationData context)
+        {
+            this.context = context;
+        }
+
+        public bool Add(Models.Product.Product product)
+        {
+            if (context.Products.FirstOrDefault(p => p.ProductID == product.ProductID) != null)
+                return false;
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return true;
+        }
+    }
+}
