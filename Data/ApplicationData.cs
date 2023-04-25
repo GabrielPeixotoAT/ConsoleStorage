28c55bd0dbb3a04ec230a50c093fb50fb0c629a2
gabrielpeixoto <gabr.art.design@gmail.com>
2023-04-24 21:40:47 -0300

    feat: testes para serialização

---
+++ Data/ApplicationData.cs
@@ -0,0 +1,62 @@
+using Newtonsoft.Json;
+using ConsoleStorage.Models.Movement;
+using ConsoleStorage.Models.Product;
+using ConsoleStorage.Models.Purchase;
+using ConsoleStorage.Models.PurchaseOrder;
+using ConsoleStorage.Models.Sale;
+using ConsoleStorage.Models.Store;
+using ConsoleStorage.Models.Supplier;
+
+namespace ConsoleStorage.Data
+{
+    public class ApplicationData
+    {
+        #region Movement
+            public List<MovementType> MovementTypes { get; set; }
+            public List<Movement> Movements { get; set; }
+        #endregion
+
+        #region Product
+            public List<Product> Products { get; set; }
+        #endregion
+
+        #region Purchase
+            public List<Purchase> Purchases { get; set; }
+            public List<PurchaseItem> PurchaseItems { get; set; }
+        #endregion
+
+        #region PurchaseOrder
+            public List<PurchaseOrder> PurchaseOrders { get; set; }
+            public List<PurchaseOrderItem> PurchaseOrderItems { get; set; }
+            public List<PurchaseOrderStatus> PurchaseOrderStatus { get; set; }
+        #endregion
+
+        #region Sale
+            public List<Sale> Sales { get; set; }
+            public List<SaleItems> SaleItems { get; set; }
+            public List<Devolution> Devolutions { get; set; }
+        #endregion
+
+        #region Store
+            public List<Store> Stores { get; set; }
+            public List<StoreAddress> StoreAddresses { get; set; }
+            public List<StorePhone> StorePhones { get; set; }
+        #endregion
+
+        #region Supplier
+            public List<Supplier> Suppliers { get; set; }
+            public List<SupplierAddress> SupplierAddresses { get; set; }
+            public List<SupplierPhone> SupplierPhones { get; set; }
+        #endregion
+
+        public void SaveChanges()
+        {
+            string jsonText = JsonConvert.SerializeObject(this);
+
+            using (StreamWriter sw = File.CreateText("data.json"))
+            {
+                sw.WriteLine(jsonText);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleStorage.Data;
+using ConsoleStorage.Models.Product;
+
+ApplicationData context = new ApplicationData();
+
+context.Products = new List<Product>() {
+    new Product {
+        ProductID = 1,
+        Name = "Teste",
+        Description = "Teste teste",
+        Code = "code",
+        Price = 14.99m,
+        Quantity = 2,
+        MinimalQuantity = 1,
+        ExpiringDate = DateTime.Now
+    },
+    new Product {
+        ProductID = 2,
+        Name = "Teste2",
+        Description = "Teste2 teste2",
+        Code = "code2",
+        Price = 9.99m,
+        Quantity = 5,
+        MinimalQuantity = 3,
+        ExpiringDate = DateTime.Now
+    }
+};
+
+context.SaveChanges();
+
+Console.WriteLine("Done");
